b2b57eaf7492f1441de7acc3880997cd369d7ced
Marc Dos Santos <mdossantosdev@gmail.com>
2018-12-02 19:24:03 +0100

    Create the structure of the app

---
+++ app.js
@@ -0,0 +1,8 @@
+// Budget Controller
+const budgetController = (() => {})();
+
+// UI Controller
+const UIController = (() => {})();
+
+// Global App Controller
+const controller = ((budgetCtrl, UICtrl) => {})(budgetController, UIController);
