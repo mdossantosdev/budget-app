3e3acffb6f1a9188d185246edb42fd72276248df
Marc Dos Santos <mdossantosdev@gmail.com>
2018-12-03 20:01:14 +0100

    Create getDOMstrings method in UIController

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const UIController = (() => {
   const DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
-    inputValue: '.add__value'
-  }
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
 
   return {
     getInput: () => {
@@ -17,6 +18,10 @@ const UIController = (() => {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    getDOMstrings: () => {
+      return DOMstrings
     }
   };
 
@@ -25,12 +30,14 @@ const UIController = (() => {
 // Global App Controller
 const controller = ((budgetCtrl, UICtrl) => {
 
+  const DOM = UICtrl.getDOMstrings();
+
   const ctrlAddItem = () => {
     // Get the field input data
     const input = UICtrl.getInput();
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
