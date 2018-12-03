c328ca0a97f93d1e1788a29d1e94e9dbf5a0c4d1
Marc Dos Santos <mdossantosdev@gmail.com>
2018-12-03 19:26:05 +0100

    Create getInput method in UIController

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 const budgetController = (() => {})();
 
 // UI Controller
-const UIController = (() => {})();
+const UIController = (() => {
+
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector('.add__type').value,
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value
+      };
+    }
+  };
+
+})();
 
 // Global App Controller
 const controller = ((budgetCtrl, UICtrl) => {
 
-  const ctrlAddItem = () => {}
+  const ctrlAddItem = () => {
+    // Get the field input data
+    const input = UICtrl.getInput();
+  }
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
